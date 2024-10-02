8376513fa279d9b4b936381bbefc8e822cc2b79d
Frank Jacobo <frankejacobos@gmail.com>
2024-10-02 17:02:03 -0500

    :sparkles: (azuiv2): add azui-copy & azui-checkbox

--- projects/azuiv2/src/public-api.ts
+++ projects/azuiv2/src/public-api.ts
@@ -10,3 +10,6 @@ export * from './lib/types';
 export * from './lib/components/azui-alert/pipes/tipo-icono-alerta.pipe';
 export * from './lib/components/azui-alert/azui-alert.component';
 export * from './lib/components/azui-alert/azui-alert.module';
+
+export * from './lib/components/azui-checkbox/azui-checkbox.component';
+export * from './lib/components/azui-copy/azui-copy.component';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { AzuiButton, AzuiAlertModule } from 'dist/azuiv2';
+import {
+  AzuiButton,
+  AzuiAlertModule,
+  AzuiCheckboxComponent,
+  AzuiCopyComponent,
+} from 'dist/azuiv2';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AzuiButton, AzuiAlertModule, HttpClientModule],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    AzuiButton,
+    AzuiAlertModule,
+    AzuiCheckboxComponent,
+    AzuiCopyComponent,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
