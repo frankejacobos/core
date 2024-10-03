7df5ef6e21c49aba1dd84478bb5b03bffb2a1096
Frank Jacobo <frankejacobos@gmail.com>
2024-10-03 14:40:25 -0500

    :sparkles: (azuiv2): add azui-divider & azui-text-area

---
+++ projects/azuiv2/src/lib/components/azui-divider/azui-divider.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'azui-divider',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './azui-divider.component.html',
+  styleUrls: ['./azui-divider.component.scss'],
+})
+export class AzuiDividerComponent {
+  /**
+   * Por defecto horizontal
+   */
+  @Input()
+  tipo: 'horizontal' | 'vertical' = 'horizontal';
+}
